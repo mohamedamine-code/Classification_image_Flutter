145ec9f987cd91d05310866d06971fe763c66ea5
mohamedamine-code <mohamedaminebensir09@gmail.com>
2025-07-11 12:30:06 +0100

    Pick Image commit

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -8,6 +11,22 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  File? image;
+  String? resulat;
+  String? Predection;
+
+  void Classification() {}
+
+  Future<void> MypickImage(ImageSource source) async {
+    final PickedFile = await ImagePicker().pickImage(source: source);
+    if (PickedFile != null) {
+      setState(() {
+        image = File(PickedFile.path);
+        Predection = "--";
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,9 +69,7 @@ class _HomepageState extends State<Homepage> {
                             color: Colors.white.withOpacity(0.9),
                           ),
                         ),
-                        const SizedBox(
-                          width: 10,
-                        ),
+                        const SizedBox(width: 10),
                         Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
@@ -77,15 +94,136 @@ class _HomepageState extends State<Homepage> {
                       ],
                     ),
                   ),
-                  const SizedBox(height: 20),
+                  const SizedBox(height: 40),
+                  GestureDetector(
+                    onTap: (){
+                      MypickImage(ImageSource.gallery);
+                    },
+                    child: Container(
+                      height: 300,
+                      // width: 350,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        gradient: LinearGradient(
+                          colors: [
+                            Colors.black.withOpacity(0.3),
+                            Colors.black.withOpacity(0.3),
+                          ],
+                        ),
+                      ),
+                      child: image == null
+                          ? Center(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(
+                                    Icons.add_a_photo,
+                                    size: 60,
+                                    color: Colors.white,
+                                  ),
+                              
+                                  const SizedBox(height: 20),
+                                  Text(
+                                    "Pick Your Image ",
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            )
+                          : ClipRRect(
+                              child: Image.file(image!, fit: BoxFit.contain),
+                            ),
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        height: 70,
+                        width: 150,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          gradient: LinearGradient(
+                            colors: [
+                              Colors.black.withOpacity(0.3),
+                              Colors.black.withOpacity(0.3),
+                            ],
+                          ),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "Resulat : ${resulat ?? "--"}",
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: 70,
+                        width: 150,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          gradient: LinearGradient(
+                            colors: [
+                              Colors.black.withOpacity(0.3),
+                              Colors.black.withOpacity(0.3),
+                            ],
+                          ),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "Predection : ${Predection ?? "--"}",
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 30),
                   Container(
-                    height: 300,
-                    // width: 350,
+                    height: 70,
+                    width: 130,
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(15),
-                      color: Colors.red,
+                      gradient: LinearGradient(
+                        colors: [
+                          const Color.fromRGBO(255, 128, 171, 1),
+                          Color(0xFF6A1B9A).withOpacity(0.8),
+                        ],
+                      ),
                     ),
-                    // child: ,
+                    child: GestureDetector(
+                      onTap: () {
+                        Classification();
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.search, color: Colors.white),
+                          Text(
+                            "Classify",
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
                 ],
               ),
